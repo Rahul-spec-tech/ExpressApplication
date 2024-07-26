8bcd5e99fc34ace2f9ff007280010f6583805169
Rahul-spec-tech <rahulrn24cse@srishakthi.ac.in>
2024-07-26 13:02:05 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express = require('express');
-const bodyParser=require('body-parser');
-const port = 8031;
-const app = express();
-app.use(bodyParser.json());
-// app.get('/home', (req, res) => {
-//     res.send('Blog Home Page');
-// });
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// app.get('/admin', (req, res) =>{
-//     res.send('Admin Page');
-// });
-app.get('/getPost', (req, res) => {
-    res.status(200).send({
-        title: "js",
-        content: "Developing using Express js framework"
-    });
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-app.post('/addPost/:id',(req, res) =>{
-    const {id}=req.params;
-    const {title}=req.body;
-    const {content}=req.body;
-    if(!title){
-        res.status(418).send({message: "The title should be mentioned"});
-    }
-    else{
-        console.log('The Id: ${id} for the Title: ${title} and its content is: ${content}');
-        res.status(201).send({ message: "Post added successfully", data: req.body });
-    }
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+
+module.exports = app;
